6f37c1bc3d1b7ef7d17e6a0df623d20e69f0faf8
[ganapat] <ganapatisakatwa@gmail.com>
2022-09-01 13:55:29 +0530

    [Ganapati]Frequency In paragraph

--- HashTable/Program.cs
+++ HashTable/Program.cs
@@ -22,6 +22,30 @@ namespace HashTable
             int frequency = linkedHashMap.Get("be");
             Console.WriteLine(frequency);
 
+            ///UC_2:Find the frequency of word in paragraph
+            Console.WriteLine("Paranoids are not"+
+                                "paranoid because they are paranoid but" +
+                                "because they keep putting themselves" +
+                                "deliberately into paranoid avoidable" +
+                                    "situations");
+            string Paragraph = "“Paranoids are not paranoid because they are " +
+               "paranoid paranoid paranoid but because they keep putting themselves deliberately into" +
+               " paranoid avoidable situations";
+            string[] letters = Paragraph.ToLower().Split(" ");
+
+            foreach (string word in letters)
+            {
+                int value = linkedHashMap.Get(word);
+                if (value == default)
+                {
+                    value = 1;
+                }
+                else value += 1;
+                linkedHashMap.Add(word, value);
+            }
+            int frequency1 = linkedHashMap.Get("paranoid");
+            Console.WriteLine(frequency1);
+
         }
     }
 }
